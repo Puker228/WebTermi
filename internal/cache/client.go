75da2f4181d8e4036680b8bc4fb0134c2eb04608
Danil <karatelanalov@yandex.ru>
2025-12-15 19:49:43 +0300

    base work with env

--- internal/cache/client.go
+++ internal/cache/client.go
@@ -1,12 +1,20 @@
 package cache
 
 import (
+	"log"
+	"os"
+
 	"github.com/redis/go-redis/v9"
 )
 
 func NewClient() *redis.Client {
+	addr, exist := os.LookupEnv("REDIS_URL")
+	if !exist {
+		log.Println("redis addres not present")
+	}
+
 	return redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     addr,
 		Password: "",
 		DB:       0,
 	})
