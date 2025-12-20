62d4e7220fe83304a2912c7cca28720bc4e3da3a
Danil <karatelanalov@yandex.ru>
2025-12-20 22:29:03 +0300

    added logs

--- internal/transport/router.go
+++ internal/transport/router.go
@@ -1,9 +1,34 @@
-// Package transport
 package transport
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func RouterRegister(e *echo.Echo, handler *SessionHandler) {
+	// added logger
+	skipper := func(c echo.Context) bool {
+		// Skip health check endpoint
+		return c.Request().URL.Path == "/health"
+	}
+
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI:    true,
+		Skipper:   skipper,
+		BeforeNextFunc: func(c echo.Context) {
+			c.Set("customValueFromContext", 42)
+		},
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			value, _ := c.Get("customValueFromContext").(int)
+			fmt.Printf("REQUEST: uri: %v, status: %v, custom-value: %v\n", v.URI, v.Status, value)
+			return nil
+		},
+	}))
+
+	// groupping api
 	apiV1 := e.Group("/api/v1")
 	apiV1.POST("/session", handler.Start)
 }
