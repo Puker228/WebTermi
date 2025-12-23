df6ef40fb67b618c70626d6fc164e3e745a76c84
Danil <karatelanalov@yandex.ru>
2025-12-24 01:37:52 +0300

    move ws router

--- internal/transport/session_handler.go
+++ internal/transport/session_handler.go
@@ -2,10 +2,12 @@
 package transport
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Puker228/WebTermi/internal/session"
 	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 )
 
@@ -28,4 +30,29 @@ func (h *SessionHandler) Start(c echo.Context) error {
 		"status": status,
 		"userID": userID,
 	})
+}
+
+var upgrader = websocket.Upgrader{}
+
+func (h *SessionHandler) Hello(c echo.Context) error {
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		// Write
+		err := ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+		if err != nil {
+			c.Logger().Error(err)
+		}
+
+		// Read
+		_, msg, err := ws.ReadMessage()
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		fmt.Printf("%s\n", msg)
+	}
 }
